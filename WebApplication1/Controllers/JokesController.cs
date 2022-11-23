e1da9f46708bdcb6ff5d0e1e3d065ab71df137c9
Acer <Acer@DESKTOP-9VNBGAB>
2022-11-23 11:58:00 +0100

    Add project files.

---
+++ WebApplication1/Controllers/JokesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.JokeModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/JokesController")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+        // GET: api/<JokesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return Ok(context.Jokes.ToList());
+        }
+
+        // GET api/<JokesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            if (keresettVicc == null)
+            {
+                return NotFound($"Nincs #{id} azonosítóval vicc");
+            }
+            else
+            {
+                return Ok(keresettVicc);
+            }
+        }
+
+        // POST api/<JokesController>
+        [HttpPost]
+        public void Post([FromBody] Joke újvicc)
+        {
+            FunnyDatabaseContext context = new();
+            context.Jokes.Add(újvicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JokesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
